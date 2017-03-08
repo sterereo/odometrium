4edf75d3bcde2cbf0f7d3d7c6b1a38df3dc8f65c
onkelheidi <x@x.x>
2017-03-08 09:40:29 +0100

    +wheel_distance, +logging (prototype)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import ev3dev.ev3 as ev3
+import time.perf_counter
 
 
 class Odometrium:
-    def __init__(self, left=None, right=None, wheel_diameter=5.5):
+    def __init__(self, left=None, right=None, wheel_diameter=5.5, wheel_distance=12):
         allowed_ports = ['A', 'B', 'C', 'D']
         if left not in allowed_ports or right not in allowed_ports:
             if left not in allowed_ports:
@@ -17,9 +18,27 @@ class Odometrium:
         # init motor wheel
         self.__motor_left = ev3.LargeMotor('out' + left)
         self.__motor_right = ev3.LargeMotor('out' + right)
+        self.__movement_logs = []
+        self.__last_log_time = time.perf_counter()
         self.stop()
 
+    def __add_log(self):
+        # generate current log
+        current_time = time.perf_counter()
+        elapsed_time = current_time - self.__last_log_time
+        new_log = {
+            'speed_left': self.speed_left,
+            'speed_right': self.speed_right,
+            'time': elapsed_time,
+        }
+        # only record log if there is movement at all
+        if 0 != self.speed_left or 0 != self.speed_right:
+            self.__movement_logs.append(new_log)
+        # set reference point for the next log
+        self.__last_log_time = time.perf_counter()
+
     def stop(self):
+        self.__add_log()
         self.__motor_left.stop()
         self.__motor_right.stop()
 
@@ -28,7 +47,6 @@ class Odometrium:
         self.__motor_right.wait_while('running')
 
     def move(self, right=0, left=0, time=None, blocking=None):
-        # if time is set: make function blocking
         self.stop()
         if time is not None:
             self.__motor_left.run_timed(time_sp=time * 1000, speed_sp=left)
@@ -47,6 +65,7 @@ class Odometrium:
                 self.wait()
 
     def change_speed(self, left=0, right=0):
+        self.__add_log()
         self.__motor_left.run_forever(speed_sp=self.speed_left + left)
         self.__motor_right.run_forever(speed_sp=self.speed_right + right)
 
@@ -65,3 +84,9 @@ class Odometrium:
     @speed_right.setter
     def speed_right(self, new_speed):
         self.change_speed(right=new_speed)
+
+    def __exit__(self):
+        self.stop()
+
+    def __del__(self):
+        pass
